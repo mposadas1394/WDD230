93cc697cc881df390656e8521cf5719a2a31985c
mposadas1394 <matiasposadas1394@gmail.com>
2024-06-25 13:57:18 -0300

    hjh

---
+++ scripts/hamButton.js
@@ -0,0 +1,22 @@
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('nav');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('navigation');
+	hamButton.classList.toggle('open');
+});
+
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("🕶️")) {
+		main.style.background = "#000";
+		main.style.color = "#fff";
+		modeButton.textContent = "🔆";
+	} else {
+		main.style.background = "#eee";
+		main.style.color = "#000";
+		modeButton.textContent = "🕶️";
+	}
+});
